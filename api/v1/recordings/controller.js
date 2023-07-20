a6ad98897f0fbb27a01d990dbf522b727034de3f
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-07-20 16:37:03 +0200

    feat: add `createRecording()` to create a recording in db

--- api/v1/recordings/controller.js
+++ api/v1/recordings/controller.js
@@ -1,5 +1,6 @@
 import FetchRequest from '../../../helpers/FetchRequest.js';
 import config from '../../../config/config.js';
+import dbConnection from '../../../dbConnection.js';
 
 const { cameraApiUrl } = config;
 
@@ -21,5 +22,26 @@ export const getRecording = (req, res) => {
         .responseNotOk((response) => res.status(response.statusCode).send(response.statusText))
         .exception((err) => res.status(500).send(err.message))
         .make();
+};
+
+export const createRecording = async (req, res) => {
+    const { recorded_at, filename, detection_session_id, camera_id } = req.body;
+
+    const sqlQuery = `
+        INSERT INTO recording (recorded_at, filename, FK_detection_session_id, FK_camera_id) VALUES (?, ?, ?, ?)
+    `;
+
+    try {
+        const [rows] = await dbConnection.execute(sqlQuery, [
+            recorded_at,
+            filename,
+            detection_session_id,
+            camera_id
+        ]);
+
+        res.type('application/json').send({ recording_id: rows.insertId });
+    } catch (e) {
+        console.log('Exception caught in createRecording():', e);
+    }
 };
 
--- api/v1/recordings/router.js
+++ api/v1/recordings/router.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getRecording, getRecordings } from './controller.js';
+import { createRecording, getRecording, getRecordings } from './controller.js';
 
 const router = express.Router();
 
 router.get('/', getRecordings);
 router.get('/:filename', getRecording);
+router.post('/', createRecording)
 
 export default router;
