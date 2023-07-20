e4d906d8c2aae8d8454a85edd538a057dcf7ab1f
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-07-20 16:40:07 +0200

    feat: add new `/detection-session` API endpoint

--- api/v1/router.js
+++ api/v1/router.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import detectionSessionsRouter from './detection_session/router.js';
 import streamingRouter from './streaming/router.js';
 import recordingsRouter from './recordings/router.js';
 
 const router = express.Router();
 
+router.use('/detection-sessions', detectionSessionsRouter);
 router.use('/streaming', streamingRouter);
 router.use('/recordings', recordingsRouter);
 
