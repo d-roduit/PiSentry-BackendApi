6ceba917e1d7fd214eb7c37764cb17dd8ee7ac9a
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-07-20 11:47:20 +0200

    feat: implement correct `responseNotOk` and `exception` callbacks

--- api/v1/recordings/controller.js
+++ api/v1/recordings/controller.js
@@ -1,23 +1,25 @@
 import FetchRequest from '../../../helpers/FetchRequest.js';
 import config from '../../../config/config.js';
 
-FetchRequest
-    .defaultResponseNotOk((response) => console.log('response not ok:', response))
-    .defaultException((err) => console.log('exception:', err));
-
 const { cameraApiUrl } = config;
 
 export const getRecordings = (req, res) => {
     new FetchRequest(`${cameraApiUrl}/recordings/`)
+        .options({ method: 'GET' })
         .success((data) => res.json(data))
+        .responseNotOk((response) => res.status(response.statusCode).send(response.statusText))
+        .exception((err) => res.status(500).send(err.message))
         .make();
 };
 
 export const getRecording = (req, res) => {
     const filename = req.params.filename;
     new FetchRequest(`${cameraApiUrl}/recordings/${filename}`)
+        .options({ method: 'GET' })
         .responseType(FetchRequest.ResponseType.ArrayBuffer)
         .success((data) => res.type('video/mp4').send(Buffer.from(data)))
+        .responseNotOk((response) => res.status(response.statusCode).send(response.statusText))
+        .exception((err) => res.status(500).send(err.message))
         .make();
 };
 
--- api/v1/streaming/controller.js
+++ api/v1/streaming/controller.js
@@ -1,16 +1,22 @@
 import FetchRequest from '../../../helpers/FetchRequest.js';
 import config from '../../../config/config.js';
 
-FetchRequest
-    .defaultResponseNotOk((response) => console.log(response))
-    .defaultException((err) => console.log(err));
-
 const { cameraApiUrl } = config;
 
-export const startStreaming = () => {
-    new FetchRequest(`${cameraApiUrl}/streaming/start`).options({ method: 'POST' }).make();
+export const startStreaming = (req, res) => {
+    new FetchRequest(`${cameraApiUrl}/streaming/start`)
+        .options({ method: 'POST' })
+        .success(() => res.status(200).end())
+        .responseNotOk((response) => res.status(response.statusCode).send(response.statusText))
+        .exception((err) => res.status(500).send(err.message))
+        .make();
 };
 
-export const stopStreaming = () => {
-    new FetchRequest(`${cameraApiUrl}/streaming/stop`).options({ method: 'POST' }).make();
+export const stopStreaming = (req, res) => {
+    new FetchRequest(`${cameraApiUrl}/streaming/stop`)
+        .options({ method: 'POST' })
+        .success(() => res.status(200).end())
+        .responseNotOk((response) => res.status(response.statusCode).send(response.statusText))
+        .exception((err) => res.status(500).send(err.message))
+        .make();
 };
