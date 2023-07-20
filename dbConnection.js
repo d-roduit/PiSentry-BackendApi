0f1fee2836fd0649c4d7afd534f6d6f32ba08d7d
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-07-20 16:35:08 +0200

    fix: return dates as string instead of date object
    Without these config values, the dates are automatically parsed and returned as date objects. Even though it could be useful in certain cases, it is a problem for our use case because it converts the date to another timezone (it removes 2 hours, so it must convert from local to UTC or something).
    By returning dates as strings, we avoid this conversion, and we can later manually make a date object with the string (`new Date(<our string date>)`) which will have no timezone issue.

--- dbConnection.js
+++ dbConnection.js
@@ -8,6 +8,10 @@ const dbConnection = mysql.createConnection({
     user: db_user,
     password: db_password,
     database: db_database,
+    dateStrings: [
+        'DATE', // DATE are returned as strings (otherwise they would be interpreted as YYYY-MM-DD 00:00:00+00:00)
+        'DATETIME' // DATETIME return as strings (otherwise they would interpreted as YYYY-MM-DD HH:mm:ss+00:00)
+    ]
 });
 
 export default dbConnection.promise();
