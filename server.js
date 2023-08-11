cfbbf2d361abb994a4fc8c8815cd2b179cce0785
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-08-11 12:13:42 +0200

    feat: set up authorization for requests made to the API

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import v1Router from './api/v1/router.js';
 import './dbConnection.js'
+import dbConnection from "./dbConnection.js";
 
 const port = process.env.PORT;
 const app = express();
@@ -15,7 +16,47 @@ const logMiddleware = (req, res, next) => {
     next();
 };
 
+const authMiddleware = async (req, res, next) => {
+    const { access_token: authorizationTokenFromUrlParams } = req.query;
+    const authorizationTokenFromHeader = req.get('authorization');
+
+    const authorizationToken = authorizationTokenFromHeader ?? authorizationTokenFromUrlParams;
+    const authorizationTokenSupplied = typeof authorizationToken !== 'undefined';
+
+    if (!authorizationTokenSupplied) {
+        console.log('Unauthorized!');
+        return res.status(401).json({ error: 'Unauthorized!' });
+    }
+
+    const sqlQuery = `
+        SELECT user_id
+        FROM user
+        WHERE token = ?
+    `;
+
+    try {
+        const [rows] = await dbConnection.execute(sqlQuery, [authorizationToken]);
+        const tokenFoundInDB = rows.length !== 0;
+
+        if (!tokenFoundInDB) {
+            console.log('Unauthorized!');
+            return res.status(401).json({ error: 'Unauthorized!' });
+        }
+
+        const [user] = rows;
+
+        req.pisentryParams = req.pisentryParams || {};
+        req.pisentryParams.authorizedUser = user;
+
+        next();
+    } catch (e) {
+        console.log('Exception caught in authMiddleware():', e);
+        return res.status(500).json({ error: 'Could not verify authorization'});
+    }
+};
+
 app.use(logMiddleware);
+app.use(authMiddleware);
 
 app.use('/v1', v1Router);
 
