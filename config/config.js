3b06fff0892aa2859e3093e9e4895a9d07d48677
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-08-16 00:58:55 +0200

    refactor: gather common properties between dev and prod config

--- config/config.js
+++ config/config.js
@@ -1,17 +1,18 @@
-const development = {
-    cameraApiUrl: 'http://192.168.1.196:9090',
+const commonProperties = {
     db_host: process.env.DB_HOST,
     db_user: process.env.DB_USER,
     db_password: process.env.DB_PASSWORD,
     db_database: process.env.DB_DATABASE,
 };
 
+const development = {
+    cameraApiUrl: 'http://192.168.1.196:9090',
+    ...commonProperties,
+};
+
 const production = {
     cameraApiUrl: 'http://127.0.0.1:4040',
-    db_host: process.env.DB_HOST,
-    db_user: process.env.DB_USER,
-    db_password: process.env.DB_PASSWORD,
-    db_database: process.env.DB_DATABASE,
+    ...commonProperties,
 };
 
 const config = process.env.NODE_ENV === 'production' ? production : development;
