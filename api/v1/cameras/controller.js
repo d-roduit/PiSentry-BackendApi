01ca054160d10cdc89812f1a539d99e123740a1d
d-roduit <37080343+d-roduit@users.noreply.github.com>
2024-01-19 19:05:15 +0100

    feat: add `getCamera` and `patchCamera` handlers

--- api/v1/cameras/controller.js
+++ api/v1/cameras/controller.js
@@ -1,4 +1,11 @@
 import dbConnectionPool from '../../../dbConnection.js';
+import {
+    isObjectEmpty,
+    checkStringType,
+    checkStringLength,
+    checkTimeFormat,
+    checkBooleanType
+} from '../../../helpers/validation.js';
 
 export const getCameras = async (req, res) => {
     const { user_id } = req.pisentryParams.authorizedUser;
@@ -13,5 +20,72 @@ export const getCameras = async (req, res) => {
     } catch (e) {
         console.log('Exception caught in getCameras():', e);
         res.status(500).json({ error: 'Could not get cameras' });
+    }
+};
+
+export const getCamera = async (req, res) => {
+    const camera = req.pisentryParams.camera;
+    res.json(camera);
+};
+
+export const patchCamera = async (req, res) => {
+    const { user_id } = req.pisentryParams.authorizedUser;
+    const { cameraId } = req.pisentryParams;
+
+    if (isObjectEmpty(req.body)) {
+        return res.status(400).json({ error: 'No fields to update provided' });
+    }
+
+    const errors= {};
+
+    let error;
+
+    for (const [key, value] of Object.entries(req.body)) {
+        switch (key) {
+            case 'camera_id':
+            case 'port':
+            case 'FK_user_id':
+                errors[key] = 'No updates allowed on this field';
+                break;
+            case 'detection_start_time':
+            case 'detection_end_time':
+            case 'notifications_start_time':
+            case 'notifications_end_time':
+                if ((error = checkStringType(value)) !== null) errors[key] = error;
+                else if ((error = checkTimeFormat(value)) !== null) errors[key] = error;
+                break;
+            case 'name':
+                if ((error = checkStringType(value)) !== null) errors[key] = error;
+                else if ((error = checkStringLength(value, 1, 35)) !== null) errors[key] = error;
+                break;
+            case 'detection_areas':
+                // validate JSON structure using a library like Ajv
+                break;
+            default:
+                errors[key] = 'Unknown field';
+                break;
+        }
+    }
+
+    if (!isObjectEmpty(errors)) {
+        return res.status(400).json({ error: errors });
+    }
+
+    const sqlQuery = `
+        UPDATE camera SET ? WHERE FK_user_id = ? AND camera_id = ?
+    `;
+
+    try {
+        /**
+         * We use `.query()` instead of `.execute()` because `.execute()` does not support taking arrays or objects as parameter.
+         * On the other hand, `.query()` does client side placeholders substitution and thus allows arrays and objects.
+         * The explanation:
+         * https://github.com/sidorares/node-mysql2/issues/553#issuecomment-437221838
+         */
+        const [rows] = await dbConnectionPool.query(sqlQuery, [req.body, user_id, cameraId]);
+        res.json(rows);
+    } catch (e) {
+        console.log('Exception caught in patchCamera():', e);
+        res.status(500).json({ error: 'Could not patch camera' });
     }
 };
--- api/v1/cameras/router.js
+++ api/v1/cameras/router.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getCameras } from './controller.js';
+import { getCameras, getCamera, patchCamera } from './controller.js';
+import { requireCameraIdToBeInteger, requireCameraToExist } from '../../../middlewares.js';
 
 const router = express.Router();
 
 router.get('/', getCameras);
+router.get('/:camera_id', requireCameraToExist, getCamera);
+router.patch('/:camera_id', requireCameraIdToBeInteger, patchCamera);
 
 export default router;
