92deaa732c05a58fbb9a4f0f3efcc78fece27dc2
d-roduit <37080343+d-roduit@users.noreply.github.com>
2024-02-06 12:30:54 +0100

    feat: use default idleTimeout (60'000ms) + enable keepAlive for db pool

--- dbConnection.js
+++ dbConnection.js
@@ -12,7 +12,7 @@ const dbConnectionPool = mysql.createPool({
         'DATE', // DATE are returned as strings (otherwise they would be interpreted as YYYY-MM-DD 00:00:00+00:00)
         'DATETIME' // DATETIME return as strings (otherwise they would interpreted as YYYY-MM-DD HH:mm:ss+00:00)
     ],
-    idleTimeout: 3600000, // idle connections' timeout. Set to 1 hour in milliseconds (60 * 60 * 1000)
+    enableKeepAlive: true,
 });
 
 export default dbConnectionPool.promise();
